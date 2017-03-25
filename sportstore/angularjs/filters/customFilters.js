2a88f84b904ed728682b4247de0cb398397cefcf
Kamil Naja <kamilnaja@gmail.com>
2017-03-25 14:16:50 +0100

    added filter

---
+++ sportstore/angularjs/filters/customFilters.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Kamil on 2017-03-25.
+ */
+angular.module("customFilters", [])
+    .filter("unique", function () {
+        //sprawdza czy dane sa w formacie tekstowym
+        return function (data, propertyName) {
+            if (angular.isArray(data) && angular.isString(propertyName)) {
+                var results = [];
+                var keys = {};
+                for (var i = 0; i < data.length; i++) {
+                    var val = data[i][propertyName];
+                    if (angular.isUndefined(keys[val])){
+                        keys[val] = true;
+                        results.push(val);
+                    }
+                }
+                return results;
+            } else {
+                return data;
+            }
+        }
+    });
